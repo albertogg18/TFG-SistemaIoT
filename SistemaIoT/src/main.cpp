fa15207c50654ffcc4def3870338562a7aafda0f
albertogg18 <albertower003@gmail.com>
2026-03-18 19:28:44 +0100

    [ADD] Implemented the reading of the BH170 sensor

--- SistemaIoT/src/main.cpp
+++ SistemaIoT/src/main.cpp
@@ -1,44 +1,71 @@
 #include <Arduino.h>
+#include <Wire.h>
 #include <DHT.h>
+#include <BH1750.h>
 
-// Definimos el pin al que conectamos el sensor (según nuestro JSON)
+// Configuracion del sensor DHT22 (Temperatura y Humedad )
 #define DHTPIN 15     
-// Definimos el tipo de sensor
 #define DHTTYPE DHT22 
 
-// Inicializamos el objeto del sensor
+// ---- Creamos el objeto de los sensores ----
+// Creacion sensor de temperatura y humedad DHT22
 DHT dht(DHTPIN, DHTTYPE);
 
+// Creacion sensor de luminosidad BH1750
+BH1750 lightMeter;
+
+// ---- Inicializacion de sensores consola ----
 void setup() {
   // Iniciamos la consola serie a 115200 baudios
   Serial.begin(115200);
   
   // Pequeña pausa para que la consola arranque limpia
   delay(1000);
-  Serial.println("--- Iniciando lectura del DHT22 ---");
   
-  // Arrancamos el sensor
+  // Arrancamos el sensor DHT22
   dht.begin();
+
+  // Arrancamos el busI2C (Pines 21 y 22 en el ESP32)
+  Wire.begin(21, 22);
+
+  // Arrancamos el sensor BH1750
+  lightMeter.begin();
 }
 
+// ---- Loop principal ----
 void loop() {
-  // El DHT22 es un sensor lento, necesita al menos 2 segundos entre lecturas
-  delay(2000);
+  // Pausa de 10 segundos entre lecturas
+  delay(10000);
 
   // Leemos la humedad y la temperatura
   float humedad = dht.readHumidity();
   float temperatura = dht.readTemperature();
 
-  // Comprobamos si hubo algún error en la lectura (isnan = Is Not a Number)
+  // Comprobamos si hubo algún error en la lectura
   if (isnan(humedad) || isnan(temperatura)) {
     Serial.println("¡Error: No se pudo leer el sensor DHT22!");
     return; // Volvemos al inicio del loop para intentar de nuevo
   }
 
-  // Imprimimos los valores por la consola
+  // Leemos el nivel de luz
+  float lightLevel = lightMeter.readLightLevel();
+
+  // Comprobamos si hubo algún error en la lectura
+  if(lightLevel < 0) {
+    Serial.println("¡Error: No se pudo leer el sensor BH1750!");
+    return; // Volvemos al inicio del loop para intentar de nuevo
+  }
+
+  /*
+  // Imprimimos los valores de la humedad y la temperatura
   Serial.print("Humedad: ");
   Serial.print(humedad);
   Serial.print("%  |  Temperatura: ");
   Serial.print(temperatura);
-  Serial.println("°C");
+  Serial.println("°C");*/
+
+  // Imprimimos el nivel de luz
+  Serial.print("Nivel de luz: ");
+  Serial.print(lightLevel);
+  Serial.println(" lx");
 }
