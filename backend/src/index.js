6ae4a0fac814a47e47380698e661e0a0c723d561
albertogg18 <albertower003@gmail.com>
2026-04-15 17:18:59 +0200

    [FIX] Updated app.listen to prepare for render.

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ app.use('/api', require('./routes/LecturasRoutes'));
 
 // Iniciar el servidor
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
   console.log('Esperando datos de sensores...');
 });
